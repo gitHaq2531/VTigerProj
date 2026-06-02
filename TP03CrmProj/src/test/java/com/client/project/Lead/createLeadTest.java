882da36fd89e225a5cdecaa5de728140da1f4945
nnakhade13 <nnakhade13@gmail.com>
2026-06-02 10:41:14 +0530

    edited lead

--- TP03CrmProj/src/test/java/com/client/project/Lead/createLeadTest.java
+++ TP03CrmProj/src/test/java/com/client/project/Lead/createLeadTest.java
@@ -1,9 +1,11 @@
 package com.client.project.Lead;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.client.ObjectRepository.LeadPage;
+import com.client.ObjectRepository.Orgpage;
 import com.client.Project.packageName.BaseClass;
 /*
  * @author : Kiran
@@ -23,10 +25,33 @@ public class createLeadTest extends BaseClass{
 		String infoMsg = ldp.getInfoMsg().getText();
 		boolean status = infoMsg.contains(lastName);
 		Assert.assertEquals(status, true);
+		
+	}
+	
+	@Test
+	public void editleadTest() throws Throwable {
+		
+		 hp.getLeadslink().click();
+	    driver.findElement(By.id("36625")).click();
+	    Orgpage op1=new Orgpage(driver);
+		op1.getEditorg().click();
+	  
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+		LeadPage ldp = new LeadPage(driver);
+		
+		String actualMsg = ldp.getEditlead().getText();
 
+		Assert.assertTrue(actualMsg.contains("Editing Lead Information"));
+
+		System.out.println("Edit message verified successfully");
+		
+	    	
+	}
+		
 		
 	}
 	
 	
+	
+	
 
-}
